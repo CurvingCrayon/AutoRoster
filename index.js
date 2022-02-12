2350f446e7a3a19ebfeefd480b08b17332c3582c
CurvingCrayon <hayden.keers@gmail.com>
2022-02-12 20:59:43 +1100

    Fixed undeclared var error

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/push', /*jsonBodyParser,*/ async (req, res) => {
     //   }
     // }
     var body = req.body;
-    data = new Buffer(body.data, 'base64');
+    var data = new Buffer(body.data, 'base64');
     data = data.toString('ascii');
     // data will be in JSON string form {"emailAddress": "user@example.com", "historyId": "9876543210"}
     data = JSON.parse(data);
